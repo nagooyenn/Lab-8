14129172fa69fdb8325855ad29458c77239a981b
Annie Nguyen <a.nguyen2@hotmail.com>
2025-04-10 04:13:37 -0400

    Part 1: Create student object and display info

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const student = {
+  name: 'Annie Nguyen',
+  age: 21,
+  enrolled: true,
+  courses: ['JavaScript', 'HTML', 'CSS'],
+  displayInfo() {
+    return `${this.name} is ${this.age} years old and is ${this.enrolled ? 'enrolled' : 'not enrolled'}.`;
+  }
+};
 
+console.log(student.name);
+console.log(student.age);
+console.log(student.displayInfo());
